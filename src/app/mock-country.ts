18592280358e3e78880a2f77274e81db77087aec
tata2019 <50076783+tata2019@users.noreply.github.com>
2023-01-02 17:46:20 -0600

    2 files modified, 2 added

---
+++ src/app/mock-country.ts
@@ -0,0 +1,24 @@
+import {Country} from './Country'
+
+export const COUNTRIES: Country[] = [ 
+  {
+    code: 'US',
+    name: 'United States'
+  },
+  {
+    code: 'CA',
+    name: 'Canada'
+  },
+  {
+    code: 'IN',
+    name: 'India'
+  },
+  {
+    code: 'MX',
+    name: 'Mexico'
+  },
+  {
+    code: 'EG',
+    name: 'Egypt'
+  }
+] 
--- src/app/travelinput/travelinput.component.ts
+++ src/app/travelinput/travelinput.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {COUNTRIES} from '../mock-country';
+import {Country} from '../Country';
 
 @Component({
   selector: 'app-travelinput',
@@ -7,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TravelinputComponent implements OnInit {
   title : string = 'Easy Travel';
+  countries: Country[] = COUNTRIES;
+
+  
   constructor() {}
 
   ngOnInit() {}
